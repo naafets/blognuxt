b9391dc38c3805ae27ec66f32cd380fe6d20fdb4
naafets <naafetss@gmail.com>
2018-03-03 04:15:26 +0100

    extract postpreview into own component

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="post-preview">
+        <div class="card">
+            <div class="card-content">
+                <div class="article-header">
+                    <nav class="level">
+                        <div class="level-left">
+                            <p class="is-size-4 ">
+                                TITLE OF THE POST
+                            </p>
+                        </div>
+                        <div class="level-right">
+                            <div class="level-item">
+                                <div class="tags has-addons">
+                                    <span class="tag">AUTHOR</span>
+                                    <span class="tag is-dark">Stefaan Sterk</span>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="is-divider" data-content="20/03/2019"></div>
+                <div class="content">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eleifend tincidunt vestibulum. Integer eu tortor id ante bibendum rhoncus eget vitae lectus. Fusce a dui sed metus varius viverra. Vestibulum luctus ex lectus, sed vulputate ex pharetra et. Sed vitae venenatis lacus, sed egestas leo. Integer convallis elit eu rhoncus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aut iure molestias, numquam odio sed iusto minima assumenda magni eveniet! Consectetur quia animi quis eligendi odio mollitia cupiditate necessitatibus sapiente.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aut iure molestias, numquam odio sed iusto minima assumenda magni eveniet! Consectetur quia animi quis eligendi odio mollitia cupiditate necessitatibus sapiente....</p>
+                    <div class="level">
+                        <div class="level-left ">
+                            <div class="level-item">
+                                <span class="tag is-rounded">Nuxt</span>
+                            </div>
+                            <div class="level-item">
+                                <span class="tag is-rounded">Blog</span>
+                            </div>
+                            <div class="level-item">
+                                <span class="tag is-rounded">Experiment</span>
+                            </div>
+                        </div>
+                        <div class="level-right ">
+                            <div class="level-item">
+                                <button class="button is-outlined readmore-button is-small has-text-weight-light ">READ MORE...</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,105 +1,27 @@
 <template>
-    <!-- <div class="box"> -->
     <div>
         <section class="level">
             <div class="level-item">
                 <p class="is-size-2 is-uppercase">Latest posts</p>
             </div>
         </section>
-        <!-- <div class="columns "> -->
-        <!-- <div class="column"> -->
-        <div class="card post-preview">
-            <div class="card-content">
-                <div class="article-header">
-                    <nav class="level">
-                        <div class="level-left">
-                            <p class="is-size-4 ">
-                                TITLE OF THE POST
-                            </p>
-                        </div>
-                        <div class="level-right">
-
-                            <div class="level-item">
-                                <div class="tags has-addons">
-                                    <span class="tag">AUTHOR</span>
-                                    <span class="tag is-dark">Stefaan Sterk</span>
-
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </nav>
-                </div>
-                <!-- <hr> -->
-                <div class="is-divider" data-content="20/03/2019"></div>
-                <div class="content is-clearfix">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eleifend tincidunt vestibulum. Integer eu tortor id ante bibendum rhoncus eget vitae lectus. Fusce a dui sed metus varius viverra. Vestibulum luctus ex lectus, sed vulputate ex pharetra et. Sed vitae venenatis lacus, sed egestas leo. Integer convallis elit eu rhoncus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aut iure molestias, numquam odio sed iusto minima assumenda magni eveniet! Consectetur quia animi quis eligendi odio mollitia cupiditate necessitatibus sapiente.</p>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aut iure molestias, numquam odio sed iusto minima assumenda magni eveniet! Consectetur quia animi quis eligendi odio mollitia cupiditate necessitatibus sapiente....</p>
-                    <!-- <p>
-                        lobortis tortor volutpat consectetur. Sed id lacus orci. Fusce efficitur risus et ex lacinia finibus. Nullam ac dolor quis nulla dapibus posuere. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed semper neque ante, ut tristique dolor lacinia sit amet. Pellentesque placerat, nulla a auctor iaculis, leo est lacinia metus, porttitor lacinia ligula erat sed nunc. Aliquam erat volutpat. Ut commodo ligula erat, sit amet aliquam neque rhoncus a. Proin convallis ipsum malesuada arcu commodo, malesuada ullamcorper felis tincidunt. Praesent nunc neque, vehicula id lectus sed, dapibus euismod nisl. Donec gravida mi et tortor mattis egestas. Phasellus blandit eros ac ornare porta. Donec finibus erat non porttitor dictum. Proin pharetra tincidunt nunc, cursus bibendum metus egestas ut. Cras id eros ex. Morbi s....
-                    </p> -->
-
-                    <div class="level">
-                        <div class="level-left ">
-                            <div class="level-item">
-                                <span class="tag is-rounded">Nuxt</span>
-                            </div>
-                            <div class="level-item">
-                                <span class="tag is-rounded">Blog</span>
-                            </div>
-                            <div class="level-item">
-                                <span class="tag is-rounded">Experiment</span>
-                            </div>
-                        </div>
-                        <div class="level-right ">
-                            <div class="level-item">
-                                <button class="button is-outlined readmore-button is-small has-text-weight-light ">READ MORE...</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-
+        <PostPreview />
+        <PostPreview />
+        <PostPreview />
     </div>
 
-    <!-- </div> -->
-    <!-- </div> -->
-    <!-- </div> -->
 </template>
 
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
 <style scoped>
-.box {
-  /* padding: 0px 8px 0px 8px; */
-}
-.level {
-}
 
-.hero {
-  height: 100vh;
-}
-
-.column {
-  /* border: 1px solid lightgray; */
-}
-
-.article-header {
-}
-
-.article-preview-content {
-  font-weight: 200;
-}
-
-.is-divider {
-  margin: 32px 0;
-}
-
-.post-preview {
-  margin-bottom: 16px;
-}
 </style>
